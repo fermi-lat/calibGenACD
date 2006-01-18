13924191f9f92b9a06872c84e26b41c7aafc5b16
echarles <>
2006-01-18 01:38:08 +0000

    remove old copy of runMuonCalib_Roi.cxx from src directory

--- src/AcdCalibVersion.h
+++ src/AcdCalibVersion.h
@@ -2,20 +2,13 @@
 #define AcdCalibVersion_h
 
 #include "Rtypes.h"
-#include <string>
 
 class AcdCalibVersion {
 
 public:
 
-  static const char* cvsName() {
-    return "$Name$";
-  }
-  
   static const char* version() {
-    const char* retVal = cvsName();
-    retVal += 7;
-    return retVal;
+    return "v1r0p0";
   }
   
 public:
