b3379bdb6d23223ccc59a9fdfec6c989ecf7d89a
ecephas <>
2008-11-03 15:55:18 +0000

    Added the following libraries:
         env.Tool('xmlBaseLib')
         env.Tool('AcdDigiLib')
         env.Tool('mootCoreLib')
         env.Tool('calibUtilLib')
         env.Tool('digiRootDataLib')
         env.Tool('addLibrary', library = env['rootLibs'])
         env.Tool('reconRootDataLib')
         env.Tool('addLibrary', library = env['rootGuiLibs'])

--- calibGenACDLib.py
+++ calibGenACDLib.py
@@ -2,7 +2,13 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibGenACD'])
-
-
+    env.Tool('xmlBaseLib')
+    env.Tool('AcdDigiLib')
+    env.Tool('mootCoreLib')
+    env.Tool('calibUtilLib')
+    env.Tool('digiRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('reconRootDataLib')
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
 def exists(env):
     return 1;
