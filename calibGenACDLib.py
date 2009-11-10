88a73dabb9de4d121d7c089adcfdf9570fbf39c6
jrb <>
2009-11-10 23:11:59 +0000

    Updates for SCons

--- calibGenACDLib.py
+++ calibGenACDLib.py
@@ -3,12 +3,15 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibGenACD'])
     env.Tool('xmlBaseLib')
-    env.Tool('AcdDigiLib')
-    env.Tool('mootCoreLib')
+    env.Tool('CalibDataLib')
+    #env.Tool('AcdDigiLib')
+    #env.Tool('mootCoreLib')
     env.Tool('calibUtilLib')
-    env.Tool('digiRootDataLib')
-    env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('reconRootDataLib')
+    env.Tool('digiRootDataLib')
+    env.Tool('commonRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('addLibrary', library = env['xercesLibs'])
 def exists(env):
     return 1;
